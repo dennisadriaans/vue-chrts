094f00b6a00710c444acce58fbb3c4953d297f1f
Dennis <adriaansendennis@gmail.com>
2025-05-03 10:59:48 +0200

    chore: barchart example

--- packages/vue/src/BarChartPage.vue
+++ packages/vue/src/BarChartPage.vue
@@ -75,6 +75,38 @@ const RevenueCategoriesMultple = {
   desktop: { name: "Desktop" },
   mobile: { name: "Mobile" },
 };
+
+const TrafficData = [
+  {
+    date: "2024-01-01",
+    country: "USA",
+    device: "Desktop",
+    users: 576,
+    unique_users: 389,
+    sessions: 7,
+    pages_per_session: 4.881913133722271,
+    scroll_depth: 56.96127813915611,
+    session_duration: 2,
+  },
+  {
+    date: "2024-01-01",
+    country: "USA",
+    device: "Mobile",
+    users: 748,
+    unique_users: 504,
+    sessions: 8,
+    pages_per_session: 4.393721820350049,
+    scroll_depth: 45.56902251132489,
+    session_duration: 2,
+  }
+]
+
+const TrafficCategories: Record<string, BulletLegendItemInterface> = {
+  users: { name: "Users", color: "#016538" },
+  unique_users: { name: "Unique users", color: "#403e3e" },
+};
+
+const xFormatter = (i: number): string | number => `${TrafficData[i]?.date}`;
 </script>
 
 <template>
@@ -217,6 +249,21 @@ const RevenueCategoriesMultple = {
         :y-formatter="(i: number) => i"
         :x-formatter="(i) => RevenueDataLong[i].date"
       />
+    </div>
+
+    <div class="max-w-7xl mx-auto">
+
+      <BarChart
+          :data="TrafficData"
+          :height="220"
+          :y-num-ticks="4"
+          :group-padding="0.2"
+           :y-axis="['users']"
+          :categories="TrafficCategories"
+          :x-formatter="xFormatter"
+          :grid-line-y="true"
+          :legend-position="LegendPosition.Bottom"
+        />
     </div>
   </div>
 </template>
