008e938cce10b6153fcf0dea1a8a07e6b7d1ed55
Dennis <adriaansendennis@gmail.com>
2025-07-05 15:12:37 +0200

    fix: try to force client only components charts

--- packages/nuxt/src/core/components.ts
+++ packages/nuxt/src/core/components.ts
@@ -2,29 +2,30 @@ import { addComponent } from "@nuxt/kit";
 import type { ModuleOptions } from "../module";
 
 export const resolveComponents = (config: ModuleOptions, filePath: string) => {
+  const { prefix } = config;
+  const allComponents = [
+    "AreaChart",
+    "AreaStackedChart",
+    "LineChart",
+    "BarChart",
+    "DonutChart",
+  ];
 
-    const { prefix } = config;
-    const allComponents = [
-        'AreaChart',
-        'AreaStackedChart',
-        'LineChart',
-        'BarChart',
-        'DonutChart'
-    ]
-
-    allComponents.forEach(component => {
-        if (typeof component === 'string') {
-            addComponent({
-                export: component,
-                name: prefix + component,
-                filePath
-            })
-        } else if (Array.isArray(component)) {
-            addComponent({
-                export: component[0],
-                name: prefix + component[1],
-                filePath
-            })
-        }
-    })
-}
+  allComponents.forEach((component) => {
+    if (typeof component === "string") {
+      addComponent({
+        export: component,
+        name: prefix + component,
+        mode: "client",
+        filePath,
+      });
+    } else if (Array.isArray(component)) {
+      addComponent({
+        export: component[0],
+        name: prefix + component[1],
+        mode: "client",
+        filePath,
+      });
+    }
+  });
+};
