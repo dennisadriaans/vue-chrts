a50135713a5b2d1bccc7116100930939502d644b
Dennis Adriaansen <adriaansendennis@gmail.com>
2025-10-21 13:15:38 +0200

    fix: type hinting line chart

--- packages/vue/lib/components/LineChart/LineChart.vue
+++ packages/vue/lib/components/LineChart/LineChart.vue
@@ -1,18 +1,19 @@
+
 <script setup lang="ts" generic="T">
 import AreaChart from "../AreaChart/AreaChart.vue";
 import type { LineChartProps } from "./types";
 
-defineOptions({
-  inheritAttrs: false,
-});
-
 const props = defineProps<LineChartProps<T>>();
+
+const slots = defineSlots<{
+  tooltip?: (props: { values: T | undefined }) => any;
+}>();
 </script>
 
 <template>
   <AreaChart v-bind="props" :hide-area="true">
-    <template v-for="(_, slot) in $slots" #[slot]="scope">
-      <slot :name="slot" v-bind="scope" />
+    <template v-if="$slots.tooltip" #tooltip="scope">
+      <slot name="tooltip" v-bind="scope" />
     </template>
   </AreaChart>
 </template>
