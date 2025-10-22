c0416214d734099a007974c5ed6fff5a203c3d21
Dennis Adriaansen <adriaansendennis@gmail.com>
2025-10-22 21:10:03 +0200

    fix: remove map files

--- packages/vue/lib/components/Maps/TopoJSONMap/types.ts
+++
@@ -1,6 +0,0 @@
-import { Topology } from "topojson-specification";
-
-export type MapsData<T extends Record<string, any>> = {
-  mapFeatureKey: keyof T;
-  data: Topology<T>;
-};
--- packages/vue/lib/index.ts
+++ packages/vue/lib/index.ts
@@ -3,7 +3,6 @@ import LineChart from "./components/LineChart/LineChart.vue";
 import BarChart from "./components/BarChart/BarChart.vue";
 import DonutChart from "./components/DonutChart/DonutChart.vue";
 import BubbleChart from "./components/BubbleChart/BubbleChart.vue";
-import TopoJSONMap from "./components/Maps/TopoJSONMap/TopoJSONMap.vue";
 import GanttChart from "./components/GanttChart/GanttChart.vue";
 
 import {
@@ -22,7 +21,6 @@ export {
     BarChart,
     DonutChart,
     BubbleChart,
-    TopoJSONMap,
     GanttChart,
     Orientation,
     CurveType,
