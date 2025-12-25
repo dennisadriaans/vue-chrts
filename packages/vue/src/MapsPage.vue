e3d4b7b38f2e0d0e71fabc06568de52ccd9db540
Dennis <adriaansendennis@gmail.com>
2025-12-25 12:04:14 +0100

    add topomap too

--- packages/vue/src/MapsPage.vue
+++ packages/vue/src/MapsPage.vue
@@ -1,7 +1,37 @@
 <script lang="ts" setup>
-import {
-  DottedWorldMap,
-} from "../lib";
+import { TopoJSONMap, DottedWorldMap } from "../lib";
+import { WorldMapTopoJSON } from "@unovis/ts/maps";
+import type { Topology, GeometryCollection } from "topojson-specification";
+import netherlandsRaw from "./data/netherlands.topo.json";
+type NetherlandsProvince = {
+  GID_1: string;
+  GID_0: string;
+  COUNTRY: string;
+  NAME_1: string;
+  VARNAME_1: string;
+  NL_NAME_1: string;
+  TYPE_1: string;
+  ENGTYPE_1: string;
+  CC_1: string;
+  HASC_1: string;
+  ISO_1: string;
+};
+const netherlands: Topology<{
+  netherlands: GeometryCollection<NetherlandsProvince>;
+}> = {
+  type: "Topology",
+  arcs: netherlandsRaw.arcs,
+  objects: {
+    netherlands: {
+      type: "GeometryCollection",
+      geometries: netherlandsRaw.objects.netherlands.geometries.map((g) => ({
+        type: "MultiPolygon",
+        arcs: g.arcs,
+        properties: g.properties,
+      })),
+    },
+  },
+};
 </script>
 
 <template>
@@ -13,16 +43,20 @@ import {
         <code>VisScatter</code>.
       </p>
 
-  <div>
-          <h3 class="text-lg font-semibold mb-2">World</h3>
-          <DottedWorldMap
-            height="600px"
-            :dot-size="1.5"
-            color="#ffffff"
-            :map-height="100"
-          />
-        </div>
+      <div>
+        <h2 class="text-2xl font-bold mb-4">TopoJSON Map</h2>
+        <TopoJSONMap :data="WorldMapTopoJSON" map-feature-key="countries" />
+      </div>
+
+      <div>
+        <h3 class="text-lg font-semibold mb-2">World</h3>
+        <DottedWorldMap
+          height="600px"
+          :dot-size="1.5"
+          color="#ffffff"
+          :map-height="100"
+        />
+      </div>
     </section>
   </div>
-
 </template>
