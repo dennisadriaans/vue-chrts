c9286f96f435f666d6344ffc2740d7f16aec0929
Dennis Adriaansen <adriaansendennis@gmail.com>
2026-01-27 07:00:42 +0100

    fix: legend example donut charts

--- packages/vue/src/DonutChartPage.vue
+++ packages/vue/src/DonutChartPage.vue
@@ -2,9 +2,20 @@
 import { DonutChart, LegendPosition } from "./../lib";
 import Card from "./elements/Card.vue";
 import { DonutChartData, DonutCategories } from "./data/DonutChartData";
+import { ref } from "vue";
 
 function handleChartClick(event: MouseEvent, hoverValues: any) {
   console.log("DonutChart clicked", hoverValues);
+}
+
+const donutData = ref([35, 25, 20, 15, 5])
+
+const marketShareLabels = {
+  'Product A': { name: 'Product A', color: 'var(--ui-primary)' },
+  'Product B': { name: 'Product B', color: 'var(--ui-info)' },
+  'Product C': { name: 'Product C', color: 'var(--ui-success)' },
+  'Product D': { name: 'Product D', color: 'var(--ui-warning)' },
+  'Other': { name: 'Other', color: 'var(--ui-error)' }
 }
 </script>
 
@@ -19,43 +30,28 @@ function handleChartClick(event: MouseEvent, hoverValues: any) {
       </div>
     </div>
     <div class="grid grid-cols-2 gap-4 max-w-screen-2xl mx-auto py-4">
-      <DonutChart
-        :data="DonutChartData"
-        :height="800"
-        :categories="DonutCategories"
-        :radius="80"
-        :pad-angle="0.5"
-        :arc-width="40"
-        :legend-position="LegendPosition.Bottom"
-        @click="handleChartClick"
-      >
+      <DonutChart :data="DonutChartData" :height="800" :categories="DonutCategories" :radius="80" :pad-angle="0.5"
+        :arc-width="40" :legend-position="LegendPosition.BottomCenter" @click="handleChartClick">
         <div class="text-center">
           <div class="font-semibold">La123123123123bel</div>
           <div class="text-muted">2 seconds ago</div>
         </div>
-        
-        </DonutChart
-      >
+
+      </DonutChart>
 
       <Card>
         <template #header>
           <h2 class="heading-2">Half Donut Chart Example</h2>
         </template>
-        <DonutChart
-          :data="DonutChartData"
-          :height="140"
-          :categories="DonutCategories"
-          :radius="0"
-          :arc-width="40"
-          :legend-position="LegendPosition.Bottom"
-          :type="'half'"
-          @click="handleChartClick"
-        >
-          <div class="pt-16">1231231231231231231234</div>
-
-          <template #tooltip="{values}">
-            {{ values.label }}
-          </template>
+        <DonutChart :data="donutData" :categories="marketShareLabels" :radius="80" :pad-angle="0.1" :arc-width="20" :legend-position="LegendPosition.BottomCenter">
+          <div class="text-center">
+            <div class="font-semibold">
+              Label
+            </div>
+            <div class="text-muted">
+              2 seconds ago
+            </div>
+          </div>
         </DonutChart>
       </Card>
     </div>
